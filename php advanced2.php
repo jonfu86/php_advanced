6a9807c49a38509936a952133367cce3ec822809
jonfu <jonfu86@gmail.com>
2013-12-03 21:27:18 -0800

    updated php advanced 2

---
+++ php advanced2.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Advanced 2</title>
+	<style type="text/css">
+	
+	table{
+		border:1px black solid;
+		border-collapse: collapse;
+	}
+	table td{
+		border:1px black solid;
+		width:70px;
+		height:70px;
+	}
+	.red{
+		background:red;
+	}
+	.black{
+		background-color:black;
+	}
+	
+
+	</style>
+
+</head>
+<body>
+	<?php
+
+	function checkers($grid){
+		echo '<table>';
+		for ($y=0; $y < $grid; $y++) 
+		{ 
+			echo '<tr>';
+			for($x=0; $x<$grid; $x++)
+			{
+				if($x % 2 !=1)
+				{
+				echo '<td class="red"></td>';
+				}
+				else
+				{
+				echo '<td class="black"></td>';
+				}
+			}
+			echo '</tr>';
+		}
+			echo '</table>';
+	}
+	checkers(8);
+
+
+	?>
+</body>
+</html>
